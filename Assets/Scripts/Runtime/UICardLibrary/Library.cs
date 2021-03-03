0ce15fb08a6252c764f1f4f1c42d6935cb44cdc6
lordbordem <lordbordem@gmail.com>
2021-03-04 03:21:32 +1100

    Force push Library.cs
    Change the folder name so it doesn't get ignored

---
+++ Assets/Scripts/Runtime/UICardLibrary/Library.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using HexCardGame.SharedData;
+using Tools.Extensions.Arrays;
+using Tools.Patterns.Observer;
+
+namespace HexCardGame.Runtime
+{
+    [Event]
+    public interface ICreateLibrary
+    {
+        void OnCreateLibrary(ILibrary library);
+    }
+
+    public interface ILibrary
+    {
+        int Size { get; }
+        CardData GetRandomData();
+        CardData GetRandomDataFromPlayer(PlayerId id);
+    }
+
+    public class Library : ILibrary
+    {
+        readonly CardData[] _register;
+        readonly Dictionary<PlayerId, CardData[]> _registerByPlayer;
+
+        public Library(Dictionary<PlayerId, CardData[]> playersLibrary, IDispatcher dispatcher)
+        {
+            Dispatcher = dispatcher;
+            _registerByPlayer = playersLibrary;
+
+            var count = 0;
+            var size = 0;
+
+            foreach (var value in _registerByPlayer.Values)
+                size += value.Length;
+
+            _register = new CardData[size];
+            foreach (var data in _registerByPlayer.Values)
+            foreach (var i in data)
+            {
+                _register[count] = i;
+                ++count;
+            }
+
+            OnCreateLibrary();
+        }
+
+        IDispatcher Dispatcher { get; }
+        public int Size => _register.Length;
+        public CardData GetRandomData() => _register.RandomItem();
+
+        public CardData GetRandomDataFromPlayer(PlayerId id) => _registerByPlayer[id].RandomItem();
+
+        void OnCreateLibrary() => Dispatcher.Notify<ICreateLibrary>(i => i.OnCreateLibrary(this));
+    }
+}
